4fb95f8f6ec607ac938156fc93bcc943ca403e01
Wazabii <daniel@creativearmy.se>
2023-03-07 21:09:07 +0100

    First commit

---
+++ lib/lib-test.php
@@ -0,0 +1,18 @@
+<?php 
+
+/**
+* Lorem ipsum dolor MacGruber
+*/
+class test  {
+
+    private $_page;
+    private $_section;
+    
+    function __construct() {
+        $this->_page = "About us"
+    }
+
+    function section() {
+        $this->_section = "Envi";
+    }
+}
